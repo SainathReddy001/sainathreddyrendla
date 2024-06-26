eae32fa8ea1bd58fcd80b8bd4bfeb83323a6838a
SainathReddy001 <saianathreddybujji@gmail.com>
2024-06-26 21:29:36 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './Home';
-import Login from './Login';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
-import Nav from './Nav';
-import About from './About';
-import Family from './Family';
-import Resume from './Resume';
-import Project0 from './Project0';
-import Project1 from './Project1';
-import Project2 from './Project2';
+
 
 
 
